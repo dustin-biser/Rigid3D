449a8042cef7b85dd901a738f867d2dec2a96c91
Dustin Biser <dustinbiser@gmail.com>
2013-07-14 11:45:56 -0400

    Adding ShaderException class

---
+++ include/utils/GlUtils/ShaderException.hpp
@@ -0,0 +1,24 @@
+#ifndef SHADEREXCEPTION_HPP_
+#define SHADEREXCEPTION_HPP_
+
+#include <exception>
+
+using namespace std;
+
+class ShaderException : public exception {
+public:
+    ShaderException(const string message = "Shader Exception Occurred.")
+        : errorMessage(message) { }
+
+    virtual ~ShaderException() noexcept { }
+
+    virtual const char * what() const noexcept {
+        return errorMessage.c_str();
+    }
+
+private:
+    string errorMessage;
+};
+
+
+#endif /* SHADEREXCEPTION_HPP_ */
