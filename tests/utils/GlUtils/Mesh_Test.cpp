ce09e5dca8a8b5409d482e8c6e549b6f10320f64
Dustin Biser <dustinbiser@gmail.com>
2013-07-17 01:32:49 -0400

    Added Mesh_Test for Mesh unit tests

---
+++ tests/utils/GlUtils/Mesh_Test.cpp
@@ -0,0 +1,54 @@
+/**
+ * \brief Mesh_Test
+ *
+ * \author Dustin Biser
+ */
+
+#include "gtest/gtest.h"
+#include "Mesh.hpp"
+#include <memory>
+
+using namespace GlUtils;
+using namespace std;
+
+namespace {  // limit class visibility to this file.
+
+    class Mesh_Test: public ::testing::Test {
+    protected:
+        shared_ptr<Mesh> mesh;
+
+        Mesh_Test() {
+            mesh = std::make_shared<Mesh>();
+        }
+
+        virtual ~Mesh_Test() {
+        }
+
+        // Code here will be called immediately after the constructor (right
+        // before each test).
+        virtual void SetUp() {
+        }
+
+        // Code here will be called immediately after each test (right
+        // before the destructor).
+        virtual void TearDown() {
+        }
+
+    };
+
+}
+
+TEST_F(Mesh_Test, test_creation_empty_vertex_data){
+    EXPECT_EQ(0, mesh->getVertexDataBytes());
+}
+
+TEST_F(Mesh_Test, test_creation_empty_normal_data){
+    EXPECT_EQ(0, mesh->getNormalDataBytes());
+}
+
+TEST_F(Mesh_Test, test_creation_empty_index_data){
+    EXPECT_EQ(0, mesh->getIndexDataBytes());
+}
+
+
+
