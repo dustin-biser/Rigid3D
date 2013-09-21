dee62f2455736fddbf00e8ae25d23aa4a1481c3c
Dustin Biser <dustinbiser@gmail.com>
2013-09-21 17:44:56 -0400

    Updated Unit Tests for MeshConsolidator
    - Added tests for constructor that takes list of const Mesh pointers

--- tests/src/utils/GlUtils/MeshConsolidator_Test.cpp
+++ tests/src/utils/GlUtils/MeshConsolidator_Test.cpp
@@ -16,11 +16,8 @@ using std::vector;
 
 namespace {  // limit class visibility to this file.
 
-    class MeshConsolidator_Test: public ::testing::Test {
+    class MeshConsolidator_Test : public ::testing::Test {
     protected:
-        static MeshConsolidator meshConsolidator;
-        static vector<BatchInfo> batchInfoVec;
-
         static const unsigned numberOfCubes = 9;
         static const unsigned sidesPerCube = 6;
         static const unsigned trianglesPerSide = 2;
@@ -28,6 +25,12 @@ namespace {  // limit class visibility to this file.
         static const unsigned floatsPerVertex = 3;
         static const unsigned normalsPerTriangle = 3;
         static const unsigned floatsPerNormal = 3;
+    };
+
+    class MeshConsolidator_WithObjFiles_Test : public MeshConsolidator_Test {
+    protected:
+        static MeshConsolidator meshConsolidator;
+        static vector<BatchInfo> batchInfoVec;
 
         // Ran once before all tests.
         static void SetUpTestCase() {
@@ -43,21 +46,46 @@ namespace {  // limit class visibility to this file.
 
             meshConsolidator.getBatchInfo(batchInfoVec);
         }
-
-        // Ran once after all tests have finished.
-        static void TearDownTestCase() {
-        }
-
     };
-
     // Define static class variables.
-    MeshConsolidator MeshConsolidator_Test::meshConsolidator;
-    vector<BatchInfo> MeshConsolidator_Test::batchInfoVec;
+    MeshConsolidator MeshConsolidator_WithObjFiles_Test::meshConsolidator;
+    vector<BatchInfo> MeshConsolidator_WithObjFiles_Test::batchInfoVec;
+
+    class MeshConsolidator_WithMeshes_Test: public MeshConsolidator_Test {
+    protected:
+        static MeshConsolidator meshConsolidator;
+        static vector<BatchInfo> batchInfoVec;
+
+        // Ran once before all tests.
+        static void SetUpTestCase() {
+            Mesh m1("../data/meshes/cube.obj");
+            Mesh m2("../data/meshes/cube.obj");
+            Mesh m3("../data/meshes/cube_smooth.obj");
+            Mesh m4("../data/meshes/cube_smooth.obj");
+            Mesh m5("../data/meshes/cube.obj");
+            Mesh m6("../data/meshes/cube.obj");
+            Mesh m7("../data/meshes/cube.obj");
+            Mesh m8("../data/meshes/cube_smooth.obj");
+            Mesh m9("../data/meshes/cube_smooth.obj");
+
+            meshConsolidator = {&m1, &m2, &m3, &m4, &m5, &m6, &m7, &m8, &m9};
+            meshConsolidator.getBatchInfo(batchInfoVec);
+        }
+    };
 
 }
 
+// Define these outside anonymous namespace so their names don't collide with
+// MeshConsolidator_WithObjFiles_Test's member variables.
+MeshConsolidator MeshConsolidator_WithMeshes_Test::meshConsolidator;
+vector<BatchInfo> MeshConsolidator_WithMeshes_Test::batchInfoVec;
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Test with .obj files
+//////////////////////////////////////////////////////////////////////////////////////////
 //---------------------------------------------------------------------------------------
-TEST_F(MeshConsolidator_Test, test_numVertexBytes){
+TEST_F(MeshConsolidator_WithObjFiles_Test, test_numVertexBytes){
 
     unsigned expectedBytes = numberOfCubes * sidesPerCube * trianglesPerSide * verticesPerTriangle
                              * floatsPerVertex * sizeof(float);
@@ -66,7 +94,7 @@ TEST_F(MeshConsolidator_Test, test_numVertexBytes){
 }
 
 //---------------------------------------------------------------------------------------
-TEST_F(MeshConsolidator_Test, test_numNormalBytes){
+TEST_F(MeshConsolidator_WithObjFiles_Test, test_numNormalBytes){
     unsigned expectedBytes = numberOfCubes * sidesPerCube * trianglesPerSide * normalsPerTriangle
                              * floatsPerNormal * sizeof(float);
 
@@ -74,34 +102,34 @@ TEST_F(MeshConsolidator_Test, test_numNormalBytes){
 }
 
 //---------------------------------------------------------------------------------------
-TEST_F(MeshConsolidator_Test, test_batchInfoVec) {
+TEST_F(MeshConsolidator_WithObjFiles_Test, test_batchInfoVec) {
     unsigned expected = numberOfCubes;
     EXPECT_EQ(expected, batchInfoVec.size());
 }
 
 //---------------------------------------------------------------------------------------
-TEST_F(MeshConsolidator_Test, test_first_batchInfo) {
+TEST_F(MeshConsolidator_WithObjFiles_Test, test_first_batchInfo) {
     BatchInfo batchInfo = batchInfoVec.at(0);
     EXPECT_EQ(0, batchInfo.startIndex);
     EXPECT_EQ(36, batchInfo.numIndices);
 }
 
 //---------------------------------------------------------------------------------------
-TEST_F(MeshConsolidator_Test, test_second_batchInfo) {
+TEST_F(MeshConsolidator_WithObjFiles_Test, test_second_batchInfo) {
     BatchInfo batchInfo = batchInfoVec.at(1);
     EXPECT_EQ(36, batchInfo.startIndex);
     EXPECT_EQ(36, batchInfo.numIndices);
 }
 
 //---------------------------------------------------------------------------------------
-TEST_F(MeshConsolidator_Test, test_third_batchInfo) {
+TEST_F(MeshConsolidator_WithObjFiles_Test, test_third_batchInfo) {
     BatchInfo batchInfo = batchInfoVec.at(2);
     EXPECT_EQ(72, batchInfo.startIndex);
     EXPECT_EQ(36, batchInfo.numIndices);
 }
 
 //---------------------------------------------------------------------------------------
-TEST_F(MeshConsolidator_Test, test_vertexDataPtr) {
+TEST_F(MeshConsolidator_WithObjFiles_Test, test_vertexDataPtr) {
     float * vertexDataPtr = const_cast<float *>(meshConsolidator.getVertexDataPtr());
 
     // Attempt to go to last float in memory block.
@@ -112,12 +140,11 @@ TEST_F(MeshConsolidator_Test, test_vertexDataPtr) {
     *vertexDataPtr += 1.0f;
     *vertexDataPtr -= 1.0f;
 
-
     ASSERT_TRUE(true);
 }
 
 //---------------------------------------------------------------------------------------
-TEST_F(MeshConsolidator_Test, test_normalDataPtr) {
+TEST_F(MeshConsolidator_WithObjFiles_Test, test_normalDataPtr) {
     float * normalDataPtr = const_cast<float *>(meshConsolidator.getNormalDataPtr());
 
     // Attempt to go to last float in memory block.
@@ -128,6 +155,83 @@ TEST_F(MeshConsolidator_Test, test_normalDataPtr) {
     *normalDataPtr += 1.0f;
     *normalDataPtr -= 1.0f;
 
+    ASSERT_TRUE(true);
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Test with Meshes
+//////////////////////////////////////////////////////////////////////////////////////////
+//---------------------------------------------------------------------------------------
+TEST_F(MeshConsolidator_WithMeshes_Test, test_numVertexBytes){
+
+    unsigned expectedBytes = numberOfCubes * sidesPerCube * trianglesPerSide * verticesPerTriangle
+                             * floatsPerVertex * sizeof(float);
+
+    EXPECT_EQ(expectedBytes, meshConsolidator.getNumVertexBytes());
+}
+
+//---------------------------------------------------------------------------------------
+TEST_F(MeshConsolidator_WithMeshes_Test, test_numNormalBytes){
+    unsigned expectedBytes = numberOfCubes * sidesPerCube * trianglesPerSide * normalsPerTriangle
+                             * floatsPerNormal * sizeof(float);
+
+    EXPECT_EQ(expectedBytes, meshConsolidator.getNumNormalBytes());
+}
+
+//---------------------------------------------------------------------------------------
+TEST_F(MeshConsolidator_WithMeshes_Test, test_batchInfoVec) {
+    unsigned expected = numberOfCubes;
+    EXPECT_EQ(expected, batchInfoVec.size());
+}
+
+//---------------------------------------------------------------------------------------
+TEST_F(MeshConsolidator_WithMeshes_Test, test_first_batchInfo) {
+    BatchInfo batchInfo = batchInfoVec.at(0);
+    EXPECT_EQ(0, batchInfo.startIndex);
+    EXPECT_EQ(36, batchInfo.numIndices);
+}
+
+//---------------------------------------------------------------------------------------
+TEST_F(MeshConsolidator_WithMeshes_Test, test_second_batchInfo) {
+    BatchInfo batchInfo = batchInfoVec.at(1);
+    EXPECT_EQ(36, batchInfo.startIndex);
+    EXPECT_EQ(36, batchInfo.numIndices);
+}
+
+//---------------------------------------------------------------------------------------
+TEST_F(MeshConsolidator_WithMeshes_Test, test_third_batchInfo) {
+    BatchInfo batchInfo = batchInfoVec.at(2);
+    EXPECT_EQ(72, batchInfo.startIndex);
+    EXPECT_EQ(36, batchInfo.numIndices);
+}
+
+//---------------------------------------------------------------------------------------
+TEST_F(MeshConsolidator_WithMeshes_Test, test_vertexDataPtr) {
+    float * vertexDataPtr = const_cast<float *>(meshConsolidator.getVertexDataPtr());
+
+    // Attempt to go to last float in memory block.
+    vertexDataPtr += meshConsolidator.getNumVertexBytes() / sizeof(float) ;
+    --vertexDataPtr;
+
+    // Attempt to modify last float in memory block.
+    *vertexDataPtr += 1.0f;
+    *vertexDataPtr -= 1.0f;
+
+    ASSERT_TRUE(true);
+}
+
+//---------------------------------------------------------------------------------------
+TEST_F(MeshConsolidator_WithMeshes_Test, test_normalDataPtr) {
+    float * normalDataPtr = const_cast<float *>(meshConsolidator.getNormalDataPtr());
+
+    // Attempt to go to last float in memory block.
+    normalDataPtr += meshConsolidator.getNumNormalBytes() / sizeof(float) ;
+    --normalDataPtr;
+
+    // Attempt to modify last float in memory block.
+    *normalDataPtr += 1.0f;
+    *normalDataPtr -= 1.0f;
 
     ASSERT_TRUE(true);
 }
