e7024bf777bba9c906c79c5e1b7059af3935e41a
Dustin Biser <dustinbiser@gmail.com>
2013-11-04 16:59:23 -0500

    Created RuntimeException class for user exceptions

---
+++ include/RuntimeException.hpp
@@ -0,0 +1,21 @@
+#ifndef RUNTIME_EXCEPTION_HPP_
+#define RUNTIME_EXCEPTION_HPP_
+
+#include <exception>
+
+class RuntimeException : public std::exception {
+public:
+    RuntimeException(const char * message = "RuntimeException Occurred.")
+        : errorMessage(message) { }
+
+    virtual ~RuntimeException() noexcept { }
+
+    virtual const char * what() const noexcept {
+        return errorMessage;
+    }
+
+private:
+    const char * errorMessage;
+};
+
+#endif /* RUNTIME_EXCEPTION_HPP_ */
