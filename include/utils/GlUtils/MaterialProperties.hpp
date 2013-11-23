aa2524b6b7bb2af9a82a96e6038d6627ca8f5e18
Dustin Biser <dustinbiser@gmail.com>
2013-11-23 14:02:04 -0500

    Minor refactoring

--- include/utils/GlUtils/MaterialProperties.hpp
+++ include/utils/GlUtils/MaterialProperties.hpp
@@ -7,6 +7,7 @@
 #ifndef MATERIALPROPERTIES_HPP_
 #define MATERIALPROPERTIES_HPP_
 
+#include "glm/glm.hpp"
 
 namespace GlUtils {
 
@@ -14,11 +15,11 @@ namespace GlUtils {
      * Material properties for emission, ambient, diffuse, and specular lighting.
      */
     struct MaterialProperties {
-        vec3 emission;  // Emission light intensity from material for each RGB component.
-        vec3 Ka;        // Coefficients of ambient reflectivity for each RGB component.
-        vec3 Kd;        // Coefficients of diffuse reflectivity for each RGB component.
-        float Ks;       // Coefficient of specular reflectivity, uniform across each RGB component.
-        float shininessFactor;   // Specular shininess factor.
+        glm::vec3 emission; // Emission light intensity from material for each RGB component.
+        glm::vec3 Ka;       // Coefficients of ambient reflectivity for each RGB component.
+        glm::vec3 Kd;       // Coefficients of diffuse reflectivity for each RGB component.
+        float Ks;           // Coefficient of specular reflectivity, uniform across each RGB component.
+        float shininessFactor;  // Specular shininess factor.
     };
 
 }
