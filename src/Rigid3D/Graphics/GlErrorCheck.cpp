6247a5964db826a2d480b3c6172e6e3b1ecb132e
dustin-biser <dustinbiser@gmail.com>
2014-12-02 18:13:13 -0500

    Update to GlErrorCheck Added method CHECK_FRAMEBUFFER_COMPLETENESS

--- src/Rigid3D/Graphics/GlErrorCheck.cpp
+++ src/Rigid3D/Graphics/GlErrorCheck.cpp
@@ -17,35 +17,64 @@ using std::endl;
 //#define GL_INVALID_VALUE                  0x0501
 //#define GL_INVALID_OPERATION              0x0502
 //#define GL_OUT_OF_MEMORY                  0x0505
+//#define GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT         0x8CD6
+//#define GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT 0x8CD7
+//#define GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER        0x8CDB
+//#define GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER        0x8CDC
+//#define GL_FRAMEBUFFER_UNSUPPORTED                   0x8CDD
+
 string getErrorString(GLenum errorCode) {
-  string result;
-  
-  switch (errorCode) {
-    case GL_NO_ERROR:
-      break;
-      
-    case GL_INVALID_ENUM:
-      result = "GL_INVALID_ENUM";
-      break;
-      
-    case GL_INVALID_VALUE:
-      result = "GL_INVALID_VALUE";
-      break;
-      
-    case GL_INVALID_OPERATION:
-      result = "GL_INVALID_OPERATION";
-      break;
-      
-    case GL_OUT_OF_MEMORY:
-      result = "GL_OUT_OF_MEMORY";
-      break;
-      
-    default:
-      break;
-  }
-  
-  return result;
-  
+    string result;
+
+    switch (errorCode) {
+        case GL_NO_ERROR:
+            break;
+
+        case GL_INVALID_ENUM:
+            result = "GL_INVALID_ENUM";
+            break;
+
+        case GL_INVALID_VALUE:
+            result = "GL_INVALID_VALUE";
+            break;
+
+        case GL_INVALID_OPERATION:
+            result = "GL_INVALID_OPERATION";
+            break;
+
+        case GL_OUT_OF_MEMORY:
+            result = "GL_OUT_OF_MEMORY";
+            break;
+
+        case GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT:
+            result = "GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT";
+            break;
+
+        case GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:
+            result = "GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT";
+            break;
+
+        case GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER:
+            result = "GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER";
+            break;
+
+        case GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER:
+            result = "GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER";
+            break;
+
+        case GL_FRAMEBUFFER_UNSUPPORTED:
+            result = "GL_FRAMEBUFFER_UNSUPPORTED";
+            break;
+
+        case GL_FRAMEBUFFER_UNDEFINED:
+            result = "GL_FRAMEBUFFER_UNDEFINED";
+            break;
+
+        default:
+            break;
+        }
+
+    return result;
 }
 
 /**
@@ -75,6 +104,16 @@ void checkGLErrors(const string &currentFileName, int currentLine) {
   if (errorFound) {
     throw Rigid3DException(errorMessage.str());
   }
+}
+
+void checkFramebufferCompleteness() {
+    GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+    if (status != GL_FRAMEBUFFER_COMPLETE) {
+        stringstream error;
+        error << "Framebuffer not complete. ";
+        error << getErrorString(status);
+        throw Rigid3DException(error.str());
+    }
 }
 
 } // namespace Rigid3D
--- src/Rigid3D/Graphics/GlErrorCheck.hpp
+++ src/Rigid3D/Graphics/GlErrorCheck.hpp
@@ -14,13 +14,17 @@
 
 #if(DEBUG)
 #define CHECK_GL_ERRORS Rigid3D::checkGLErrors(__FILE__, __LINE__)
+#define CHECK_FRAMEBUFFER_COMPLETENESS Rigid3D::checkFramebufferCompleteness()
 #else
 #define CHECK_GL_ERRORS
+#define CHECK_FRAMEBUFFER_COMPLETENESS
 #endif
 
 namespace Rigid3D {
 
     void checkGLErrors(const std::string & currentFileName, int currentLineNumber);
+
+    void checkFramebufferCompleteness();
 
 }
 
